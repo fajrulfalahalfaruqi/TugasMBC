560de2fdd62ad4c5693df247853b61fa2e640b55
andradarmawan <72918247+andradarmawan@users.noreply.github.com>
2022-11-03 21:38:51 +0700

    Update kubusvol.py

--- kubusvol.py
+++ kubusvol.py
@@ -2,8 +2,8 @@ def volume_kubus(p,l,t):
     hasil=(p*l*t)
     return hasil
 
-p=int(input("Masukkan nilai panjang:"))
-l=int(input("Masukkan nilai lebar:"))
-t=int(input("Masukkan nilai tinggi:"))
+p=int(input("Masukkan Nilai Panjang:"))
+l=int(input("Masukkan Nilai Lebar:"))
+t=int(input("Masukkan Nilai Tinggi:"))
 
 print("Volume Kubus", volume_kubus(p,l,t))
